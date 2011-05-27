7550cc34c1d33ed668e76b1588040f58a7029cc9
Michael Kerrin <michael.kerrin@gmail.com>
2011-05-27 11:44:14 +0100

    Simpilify the dependency management.

--- calc_deps.sh
+++ calc_deps.sh
@@ -1,1 +1,2 @@
-./closure-library/closure/bin/build/depswriter.py --root=. > deps.js
+# ./closure-library/closure/bin/build/depswriter.py --root=. > deps.js
+./closure-library/closure/bin/calcdeps.py --output_mode deps --dep ./closure-library --path mvc > mvc-deps.js
